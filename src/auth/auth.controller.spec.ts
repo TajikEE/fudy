35bee3e5fa5fe4fcdad3371896119f853b488e84
Tajik Ifrad <tajik.ifrad@siteminder.com>
2023-07-12 00:53:32 +0300

    tests for auth controller and service

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,20 +1,47 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { JwtAuthGuard } from './jwt-auth.guard';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { LoginResDto } from './dto/login-response.dto';
+import { GetMeResDto } from './dto/get-me-response.dto';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
+  let authService: AuthService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [AuthController],
-      providers: [AuthService],
-    }).compile();
-
-    controller = module.get<AuthController>(AuthController);
+  beforeEach(() => {
+    authService = new AuthService(null, null);
+    authController = new AuthController(authService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('login', () => {
+    it('should return the result from authService.login', async () => {
+      const loginDto: LoginDto = {
+        email: 'test@example.com',
+        password: 'password',
+      };
+      const loginResDto: LoginResDto = { statusCode: 200, token: 'token' };
+
+      jest.spyOn(authService, 'login').mockResolvedValueOnce(loginResDto);
+
+      const result = await authController.login(loginDto);
+
+      expect(authService.login).toHaveBeenCalledWith(loginDto);
+      expect(result).toBe(loginResDto);
+    });
+  });
+
+  describe('getMe', () => {
+    it('should return the result from authService.getMe', async () => {
+      const email = 'test@example.com';
+      const getMeResDto: GetMeResDto = { statusCode: 200, id: 1, email };
+
+      jest.spyOn(authService, 'getMe').mockResolvedValueOnce(getMeResDto);
+
+      const result = await authController.getMe({ user: { email } });
+
+      expect(authService.getMe).toHaveBeenCalledWith(email);
+      expect(result).toBe(getMeResDto);
+    });
   });
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,89 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+
 import { AuthService } from './auth.service';
+import { UserService } from '../user/user.service';
+import { TestingModule, Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '../user/entities/user.entity';
+import { Repository } from 'typeorm';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let userService: UserService;
+  let jwtService: JwtService;
+  let userRepository: Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        UserService,
+        JwtService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    userService = module.get<UserService>(UserService);
+    jwtService = module.get<JwtService>(JwtService);
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('login', () => {
+    it('should return a token if login is successful', async () => {
+      const loginDto = { email: 'test@example.com', password: 'password' };
+      const user = {
+        id: 1,
+        email: loginDto.email,
+        password: 'hashedPassword',
+      };
+
+      jest
+        .spyOn(authService as any, 'validateUser')
+        .mockResolvedValueOnce(user);
+      jest
+        .spyOn(authService as any, 'generateJwtToken')
+        .mockReturnValueOnce('token');
+
+      const result = await authService.login(loginDto);
+
+      expect((authService as any).validateUser).toHaveBeenCalledWith(
+        loginDto.email,
+        loginDto.password,
+      );
+      expect((authService as any).generateJwtToken).toHaveBeenCalledWith(user);
+      expect(result).toEqual({ statusCode: 200, token: 'token' });
+    });
+
+    it('should throw UnauthorizedException if login fails', async () => {
+      const loginDto = { email: 'test@example.com', password: 'password' };
+
+      jest
+        .spyOn(authService as any, 'validateUser')
+        .mockResolvedValueOnce(null);
+
+      await expect(authService.login(loginDto)).rejects.toThrowError(
+        UnauthorizedException,
+      );
+    });
+  });
+
+  describe('getMe', () => {
+    it('should return the user details', async () => {
+      const email = 'test@example.com';
+      const user = { id: 1, email, password: 'hashedPassword' };
+
+      jest.spyOn(userService, 'findUserByEmail').mockResolvedValueOnce(user);
+
+      const result = await authService.getMe(email);
+
+      expect(userService.findUserByEmail).toHaveBeenCalledWith(email);
+      expect(result).toEqual({ statusCode: 200, id: 1, email });
+    });
   });
 });
