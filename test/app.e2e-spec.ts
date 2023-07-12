8dd65d9e1d62b8bcb83ebbe1dbedc926b6dd91b8
Tajik Ifrad <tajik.ifrad@siteminder.com>
2023-07-12 11:15:30 +0300

    update e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,51 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
+
+import * as jwt from 'jsonwebtoken';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe('User and Auth modules (e2e)', () => {
+  const randomEmail = `${Math.random()
+    .toString(36)
+    .substring(2, 15)}@example.com`;
+  const mockUser = {
+    email: randomEmail,
+    password: 'Asdqwe1@',
+  };
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+  describe('/users (POST)', () => {
+    const userUrl = `http://localhost:4005/users/`;
+    it('it should create a user and return id, statusCode, email ', () => {
+      return request(userUrl)
+        .post('')
+        .set('Accept', 'application/json')
+        .send(mockUser)
+        .expect((response: request.Response) => {
+          const { id, statusCode, email } = response.body;
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+          expect(typeof statusCode).toBe('number'),
+            expect(typeof id).toBe('number'),
+            expect(statusCode).toEqual(201),
+            expect(email).toEqual(mockUser.email);
+        })
+        .expect(HttpStatus.CREATED);
+    });
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('/auth/login (POST)', () => {
+    const authUrl = `http://localhost:4005/auth/`;
+    it('it should login a user and return a token, statusCode', () => {
+      return request(authUrl)
+        .post('login')
+        .set('Accept', 'application/json')
+        .send(mockUser)
+        .expect((response: request.Response) => {
+          const { token, statusCode } = response.body;
+
+          expect(typeof statusCode).toBe('number'),
+            expect(statusCode).toEqual(201),
+            expect(token).toBeDefined();
+        })
+        .expect(HttpStatus.CREATED);
+    });
   });
 });
